07f379c715799094a3c6501bd8576b0d7c94d444
Moriarty123 <1561357687@qq.com>
2018-12-11 10:59:31 +0800

    2018-12-11 shixunTP5
    Signed-off-by: Moriarty123 <1561357687@qq.com>

---
+++ ThinkPHP/application/index/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        return 'hello world';
+    }
+
+    public function test()
+    {
+        return 'test';
+    }
+}

9ed6706fae982a81429e1b2d667a497eef150f71
Moriarty123 <1561357687@qq.com>
2018-12-19 17:40:49 +0800

    2018-12-20 detail
    Signed-off-by: Moriarty123 <1561357687@qq.com>

--- ThinkPHP/application/index/controller/Topic.php
+++ ThinkPHP/application/index/controller/Topic.php
@@ -1,33 +1,26 @@
 <?php
 namespace app\index\controller;
-
 use think\Db;
 use think\Controller;
-
+use think\Request;
 class Topic extends Controller
 {
     public function index()
     {
         $topicList = db('topic')->paginate(3);
-
         $this->assign('topicList', $topicList);
-
         return $this->fetch('topicList');
     }
-
     public function topic()
     {
     	return $this->fetch('topic');
     }
     
-
     public function getAllTopic()
     {
     	$ret = db('topic')->select();
-
     	return $ret;
     }
-
     public function getPostList()
     {
         $topic_id = input('get.topic');
@@ -35,13 +28,10 @@ class Topic extends Controller
        //获取特定话题的帖子
        $post = new Post();
        $postList = $post->getTopicPost($topic_id);
-
        // dump($postList);
-
        $this->assign('postList', $postList);
        return $this->fetch('postList');
     }
-
     //打开帖子详细信息
     public function getPostDetail()
     {
@@ -51,23 +41,21 @@ class Topic extends Controller
         //查询该id的帖子
         $Post = new Post();
         $post = $Post->getOnePost($id);
-
         
         //2.判断查看帖子次数，存入post_access表
-        // $data = [
-        //     'post_id'   =>  $post['id'],
-        //     'ip'        =>  $request->ip();
-        //     'addTime'   =>  time();  
-        // ];
-        // $ret = db('post_access')->insert($data);
-
-        // $count = db('post_access')->count('ip');
-
+        $request = Request::instance();
+        $data = [
+            'post_id'   =>  $post['id'],
+            'ip'        =>  $request->ip(),
+            'addTime'   =>  time() 
+        ];
+        $ret = db('post_access')->insert($data);
+        $count = db('post_access')->count('ip');
+        // dump($count);
+        $this->assign('readnum', $count);
         //3.判断点赞次数
         //
-
         $this->assign('post', $post);
-
         return $this->fetch('detail');
     }
 }
